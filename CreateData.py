092660d599cce42cb6c4a0d9be95c542b0c596bb
nonowar7 <sabalanoam77@gmail.com>
2022-10-26 23:34:28 +0300

    Added data creation for algos and modified test

--- CreateData.py
+++ CreateData.py
@@ -2,15 +2,37 @@
 #import packages
 import argparse
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-s', '--SaveData', required=True, type=str, help='path to save data file')
-    parser.add_argument('-n', '--NumPoints', default=100, type=int)
+    parser.add_argument('-n', '--NumPoints', default=200, type=int)
     parser.add_argument('-d', '--DimPoints', default=2, type=int)
     args = parser.parse_args()
 
     N, D = args.NumPoints, args.DimPoints
+
+    # for kmeans
     data_points = np.random.rand(N, D)
+
+    # for dbscan
+    lower, upper = 0.4, 0.6
+    data_points = np.random.uniform(lower,upper,[N//5,D])
+    while True:
+        outer_point = np.random.uniform(0, 1, [1, D])
+        if any(d > upper+0.2 or d < lower-0.2 for d in outer_point[0]):
+            data_points = np.concatenate((data_points, outer_point))
+        if data_points.shape[0] >= N:
+            break
+
     np.save(args.SaveData, data_points)
+
+    # plot data
+    df = pd.DataFrame(data_points, columns=[0, 1])
+    ax = sns.scatterplot(data=df, x=0, y=1)
+    ax.get_figure().savefig('check.png', bbox_inches='tight')
+    plt.clf()
--- Test.py
+++ Test.py
@@ -3,11 +3,13 @@
 import argparse
 import numpy as np
 from Algorithms.KMeans import KMeans
+from Algorithms.DBScan import DBScan
 import os
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-a', '--Algorithm', required=True, type=str, help='either KMeans OR DBScan')
     parser.add_argument('-k', '--NumCentroids', default=2, type=int)
     parser.add_argument('-f', '--FiguresDir', default='Figures', type=str)
     parser.add_argument('-d', '--Data', required=True, help='path to data file, npy')
@@ -22,4 +24,10 @@ if __name__ == "__main__":
             os.remove(file_name)
 
     data_points = np.load(args.Data)
-    KMeans(K=args.NumCentroids, data_points=data_points, figures_dir=args.FiguresDir).run()
+
+    if args.Algorithm == 'KMeans':
+        KMeans(data_points=data_points, K=args.NumCentroids, figures_dir=args.FiguresDir).run()
+    elif args.Algorithm == 'DBScan':
+        DBScan(data_points=data_points, close_bound=5, radios=0.2, figures_dir=args.FiguresDir).run()
+    else:
+        raise ValueError("unrecognized alogrithm {}".format(args.Algorithm))
