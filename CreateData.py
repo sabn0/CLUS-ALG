d63892f2a28de0bda84f070341f911d16373e9f9
nonowar7 <sabalanoam77@gmail.com>
2023-06-05 10:27:37 +0300

    For known algorithms

--- CreateData.py
+++ CreateData.py
@@ -10,25 +10,28 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-s', '--SaveData', required=True, type=str, help='path to save data file')
+    parser.add_argument('-a', '--Algorithm', required=True, type=str, help='name of algorithm')
     parser.add_argument('-n', '--NumPoints', default=200, type=int)
     parser.add_argument('-d', '--DimPoints', default=2, type=int)
     args = parser.parse_args()
 
     N, D = args.NumPoints, args.DimPoints
 
-    # for kmeans
-    data_points = np.random.rand(N, D)
+    if args.Algorithm == 'KMeans':
+        # for kmeans
+        data_points = np.random.rand(N, D)
+    elif args.Algorithm == 'DBScan':
+        # for dbscan
+        lower, upper = 0.4, 0.6
+        data_points = np.random.uniform(lower,upper,[N//5,D])
+        while True:
+            outer_point = np.random.uniform(0, 1, [1, D])
+            if any(d > upper+0.2 or d < lower-0.2 for d in outer_point[0]):
+                data_points = np.concatenate((data_points, outer_point))
+            if data_points.shape[0] >= N:
+                break
+    else:
+        raise ValueError("unrecognized alogrithm {}".format(args.Algorithm))
 
-    # for dbscan
-    """
-    lower, upper = 0.4, 0.6
-    data_points = np.random.uniform(lower,upper,[N//5,D])
-    while True:
-        outer_point = np.random.uniform(0, 1, [1, D])
-        if any(d > upper+0.2 or d < lower-0.2 for d in outer_point[0]):
-            data_points = np.concatenate((data_points, outer_point))
-        if data_points.shape[0] >= N:
-            break
-    """
-
+    
     np.save(args.SaveData, data_points)
